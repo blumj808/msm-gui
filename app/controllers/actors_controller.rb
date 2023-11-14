087ba9b06c3fa6e09c7783983876d4e5df5ddea1
blumj808 <blumenfeld.jake@gmail.com>
2023-11-14 00:32:25 +0000

    save

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -15,7 +15,6 @@ class ActorsController < ApplicationController
     render({ :template => "actor_templates/show" })
   end
 
-
   def create
     # params hash looks like this:
     # {"the_title"=>"1", "the_year"=>"2", "the_duration"=>"3", "the_description"=>"4", "the_image"=>"5", "the_director_id"=>"6"}
@@ -42,26 +41,24 @@ class ActorsController < ApplicationController
 
   def update
     # Get the ID out of params
-    m.id = params.fetch("actor_id")
+    m_id = params.fetch("actor_id")
 
     # Look up the existing record
     matching_records = Actor.where({ :id => m_id })
     the_actor = matching_records.at(0)
 
     # Overwrite each column with the values from user inputs
-    the_movie.title = params.fetch("the_title")
-    the_movie.year = params.fetch("the_year")
-    the_movie.duration = params.fetch("the_duration")
-    the_movie.description = params.fetch("the_description")
-    the_movie.image = params.fetch("the_image")
-    the_movie.director_id = params.fetch("the_director_id")
-
+    m.name = params.fetch("the_name")
+    m.dob = params.fetch("dob")
+    m.bio = params.fetch("the_bio")
+    m.image = params.fetch("the_image")
+    m.save
     # Save
 
     the_movie.save
 
     # Redirect to the movie details page
-    redirect_to("/movies/#{the_movie.id}")
+    redirect_to("/actors/#{the_movie.id}")
   end
 
 end
